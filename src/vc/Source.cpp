fff9840f1a126a0bd4200b2b8589c5ec0e827c0c
Decommissioned <christian.k.gomes@live.com>
2015-05-01 05:25:58 -0300

    Simple test program

--- src/vc/Source.cpp
+++ src/vc/Source.cpp
@@ -1,52 +1,28 @@
 #include <SDL2/SDL.h>
 #include <GL/glew.h>
 
-bool run = true;
+#include "../common/display.h"
 
-void MakeWindow()
+int main(int argc, char**argv)
 {
-        SDL_Window* wnd;
-        SDL_GLContext context;
 
-        SDL_Init(SDL_INIT_VIDEO);
-                
-        wnd = SDL_CreateWindow("TEST MESSAGE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
-
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
-        context = SDL_GL_CreateContext(wnd);
-        
-        auto ret = glewInit();
-
+        orc::Display d("TEST MESSAGE", 800, 600);
         glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
 
-        SDL_ShowWindow(wnd);
-
-        while (run)
+        SDL_Event e;
+        bool quit = false;
+        while (!quit)
         {
-                SDL_Event e;
                 if (SDL_PollEvent(&e))
                 {
-                        switch (e.type)
-                        {
-                        case SDL_QUIT: run = false;
-                        }
+                        quit = (e.type == SDL_QUIT);
                 }
                 else
                 {
                         glClear(GL_COLOR_BUFFER_BIT);
-                        SDL_GL_SwapWindow(wnd);
+                        d.Present();
                 }
         }
-
-        SDL_DestroyWindow(wnd);
-        SDL_Quit();
-}
-
-int main(int argc, char**argv)
-{
-        MakeWindow();
+        
         return 0;
 }
