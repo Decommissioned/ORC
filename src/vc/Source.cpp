fe3b462a2c0020add71ecb645e70ae03873ee434
Decommissioned <christian.k.gomes@live.com>
2015-05-19 04:39:27 -0300

    Altered test program to track a monkey around the map and WASD movement

--- src/vc/Source.cpp
+++ src/vc/Source.cpp
@@ -17,11 +17,17 @@ extern void HideConsoleWindow();
 
 #include <iostream>
 
+#include "../common/camera.h"
 #include "../common/renderer.h"
 
 //////////////////////////////////////////////////////////////////////////
 
 using DM = orc::DisplayManager;
+
+orc::Renderer* renderer;
+orc::Camera camera;
+
+glm::vec3 interest = {0.0f, 0.0f, 0.0f};
 
 void MouseButtonHandler(orc::uint8 button, bool down)
 {
@@ -30,22 +36,36 @@ void MouseButtonHandler(orc::uint8 button, bool down)
 
 void MouseMotionHandler(orc::int16 dx, orc::int16 dy)
 {
-        
+        camera.Roll(dx * -0.007f);
+        camera.Pitch(dy * 0.004f);
 }
 
-void MouseWheelHandler(orc::int16 m)
+void MouseWheelHandler(orc::int16 direction)
 {
-        
+        camera.IncDistance(float(direction) * 0.25f);
 }
 
 void KeyboardHandler(char key, bool down)
 {
+        glm::vec3 last = interest;
 
+        const float speed = 0.1f;
+        switch (key)
+        {
+        case 'w': interest.z += speed; break;
+        case 's': interest.z -= speed; break;
+        case 'a': interest.x += speed; break;
+        case 'd': interest.x -= speed; break;
+        }
+
+        camera.Move(interest - last);
 }
 
 void Render(orc::uint32 windowID, float r, float g, float b)
 {
         DM::CreateOpenGLContext(windowID);
+
+        renderer = new orc::Renderer();
 
         DM::AddMouseMovementHandler(windowID, MouseMotionHandler);
         DM::AddMouseWheelHandler(windowID, MouseWheelHandler);
@@ -54,39 +74,47 @@ void Render(orc::uint32 windowID, float r, float g, float b)
 
         glClearColor(r, g, b, 1.0f);
 
-        orc::Renderer renderer;
-
         std::vector<orc::Entity*> ents;
-        ents.reserve(100);
+        ents.reserve(101);
         for (int i = 0; i < 100; i++)
         {
-                if (i % 3 == 0) ents.emplace_back(&renderer.AddEntity("monkey", "jade"));
-                else if (i % 2 == 0) ents.emplace_back(&renderer.AddEntity("peon", "marble"));
-                else ents.emplace_back(&renderer.AddEntity("sphere", "jade"));
+                ents.emplace_back(&renderer->AddEntity("sphere", "marble"));
         }
+
+        ents.emplace_back(&renderer->AddEntity("monkey", "dome"));
+        orc::Entity* player = ents.back();
 
         PRINT_LAST_ERROR;
 
         float t= 0.0f;
         while (!DM::ExitRequested)
         {
+                //camera.Track(interest);
+                renderer->View(camera.View());
+
                 for (int i = 0; i < 100; i++)
                 {
-                        ents[i]->transform.Translate((-5 + i / 10) * 3, (-5 + i % 10) * 3, 15.0f);
-                        ents[i]->transform.Rotate(i * 0.02f, t, t * 2.0f - i * 0.05f);
+                        ents[i]->transform.Translate((-5 + i / 10) * 14 + 2.5, 4 * (sinf(i) + 1.0f), (-5 + i % 10) * 14);
+                        ents[i]->transform.Rotate(0.0f, t * (i + 1.0f) / 100.0f, 0.0f);
                 }
+                auto pos = camera.Position();
+                player->transform.Translate(pos.x, pos.y, pos.z);
+                player->transform.Rotate(0.0f, camera.Roll(), 0.0f);
 
-                renderer.Update(0.016f);
+                renderer->Update(0.016f);
 
                 for (auto entity : ents)
                 {
                         entity->transform.LoadIdentity();
                 }
+                player->transform.LoadIdentity();
 
                 DM::Present(windowID);
                 t += 0.01f;
         }
         DM::DestroyWindow(windowID);
+
+        delete renderer;
 }
 
 int main(int argc, char**argv)
